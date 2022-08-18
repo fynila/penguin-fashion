314ad386ed342e3df88db39afe012deeef4186e0
fynila <neelaahmed530@gmail.com>
2022-08-18 14:54:30 +0600

    h

---
+++ penguin.js
@@ -0,0 +1,4 @@
+document.getElementById('penguin-top-button').addEventListener('click', function(){
+    const titleName = document.getElementById('penguin-text');
+    titleName.innerText = 'I am The Penguin';
+})
